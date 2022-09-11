67d09771154e1a741d99c7f3de38e929cfa3c4fb
Geoffrey Pkorkor <jeffahpk@gmail.com>
2022-09-11 13:45:26 -0700

    This program displays a string demonstrating the use of escape characters

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - This program displays a string
+ * 
+ *Return: 1 (True)
+ */
+
+int main(void)
+{
+  puts("and that piece of art is useful\" - Dora Korpar. 2015-10-19");
+  return (1);
+}
