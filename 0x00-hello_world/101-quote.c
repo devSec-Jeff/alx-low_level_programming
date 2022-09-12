e34cd1b032334bcb121c1c211452ad3c818e67b5
Geoffrey Pkorkor <jeffahpk@gmail.com>
2022-09-12 12:09:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - This program prints a sentence and a date,
- * followed by a new line, to the standard error
+ * followed by a new line, to the stderr
  * Return: 1 for success
  */
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
