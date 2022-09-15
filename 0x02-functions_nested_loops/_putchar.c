d5edbad95c3f427c3057797c7b2e412f37287291
Geoffrey Pkorkor <32089359+Jeffah@users.noreply.github.com>
2022-09-15 03:52:59 +0300

    Last commit changes

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -8,10 +8,14 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 <<<<<<< HEAD
+int _putchar(char c)
+=======
+<<<<<<< HEAD
 int  _putchar(char *c)
 =======
 int _putchar(char c)
 >>>>>>> f64e52fd4f40c49041e42f64846c2f3941a119c3
+>>>>>>> 0fed5f6532e46d3b108810091c8c1c928a78eb46
 {
   return (write(1, &c, 1));
 }
