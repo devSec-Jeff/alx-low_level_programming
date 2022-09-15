8f556fc8acc6d340aadf5040afce250b680e678a
Geoffrey Pkorkor <32089359+Jeffah@users.noreply.github.com>
2022-09-16 00:56:05 +0300

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,16 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9
- * followed by a new line
- * Return: Always 0 (Success)
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	for (int i = 0; i < 10; i++)
+	int num;
+
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar(i + '0');
+		_putchar(num  + '0');
 	}
 
 	_putchar('\n');
