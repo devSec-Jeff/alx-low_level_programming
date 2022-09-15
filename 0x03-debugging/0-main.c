c59b353c21f9e0af0b047bcf12336eb1dff9323d
Geoffrey Pkorkor <jeffahpk@gmail.com>
2022-09-15 06:11:55 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,21 +3,23 @@
 /**
  * positive_or_negative - prints if an intger is positive or
  * negative
- * @i - integer parameter
+ * @i: integer parameter
  * Return: 0
  */
 void positive_or_negative(int i)
 {
 	int num = i;
 
-	if (num < 0 )
+	if (num < 0)
 	{
-		printf("%d, is negative", num);
+		printf("%d, is negative\n", num);
 	}
 	else if (num > 0)
 	{
-		printf("%d, is positive", num);
+		printf("%d, is positive\n", num);
 	}
 	else
-		printf("%d, is zero", num);
+		printf("%d, is zero\n", num);
+
 }
+
