c1716defae161591c1a70143b0afa22b6214ff10
Geoffrey Pkorkor <jeffahpk@gmail.com>
2022-09-15 14:05:09 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,7 +8,7 @@
 
 int _isdigit(int c)
 {
-	if (isdigit(c) == 1)
+	if (c >= 48 && c == 57)
 	{
 		return (1);
 	}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <ctype.h>
 
 int _isupper(int c);
 
