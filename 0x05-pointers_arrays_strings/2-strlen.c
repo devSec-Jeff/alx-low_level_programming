adcb77c6c3616150f76c4f034549601dfa0ec3d3
Geoffrey Pkorkor <jeffahpk@gmail.com>
2022-09-19 15:49:00 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - function returning lenght of a string
+ * @s: character string
+ * Return: length of a string (int)
+ */
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
