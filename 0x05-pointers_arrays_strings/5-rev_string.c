21482a43f37ee65cb1ef09d4b0235d16f150ce28
Geoffrey Pkorkor <32089359+Jeffah@users.noreply.github.com>
2022-09-20 02:52:20 +0300

    9-strcpy.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,35 @@
-Text
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: params
+ *
+ * Return: void return type
+*/
+
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
