aeb7332b5fa2554dba95a1dd921cc9718c0cf579
Geoffrey Pkorkor <jeffahpk@gmail.com>
2022-09-21 14:07:10 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: the first character string pointer
+ * @src: the second character string pointer
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest = dest + src;
+	return (dest);
+}
