e0ebc4cedcb12b69d20017d7e16387a29527f259
Geoffrey Pkorkor <jeffahpk@gmail.com>
2022-09-22 07:20:44 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,26 +8,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1)
+	while (*s1 && (*s1 == *s2))
 	{
-		/*
-		 * if characters differ, or end of
-		 * the second string is reached
-		 */
-		if (*s1 != *s2)
-		{
-			break;
-		}
-		/*
-		 * move to the next pair of
-		 * characters
-		 */
 		s1++;
 		s2++;
 	}
-	/*
-	 * return the ASCII difference after converting
-	 * 'char*' to 'unsigned char*'
-	 */
-	return (*(unsigned char)*s1 - *(unsigned char)*s2);
+	return (*(unsigned char)s1 - *(unsigned char)s2);
 }
